26cebff2c6403a6f0de13038932fa527581c46d0
andre4ik3 <andre4ik3@fastmail.com>
2025-02-26 08:01:50 +0400

    firmware: extract Wi-Fi firmware from linux-firmware's board-2.bin

--- packages/firmware/default.nix
+++ packages/firmware/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, cabextract, rsync, fetchurl }:
+{ python3, stdenv, cabextract, rsync, fetchurl, linux-firmware }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "denali-firmware";
@@ -20,13 +20,21 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-TuS/PPCXvbBakCq7nsRpODHyC2nj90l/V6TIxyL3Cy8=";
   };
 
+  board-2 = "${linux-firmware}/lib/firmware/ath12k/WCN7850/hw2.0/board-2.bin";
+
+  bdencoder = fetchurl {
+    url = "https://raw.githubusercontent.com/qca/qca-swiss-army-knife/f2164085920540f4ecbfa0b12959918c601724b6/tools/scripts/ath12k/ath12k-bdencoder";
+    hash = "sha256-hzn/GVo7nZmuBpuBsEZUcf928w03cgANq+kaUlGmeYA=";
+  };
+
   nativeBuildInputs = [
     cabextract
     rsync
+    python3
   ];
 
   buildCommand = ''
-    mkdir -p $out/lib/firmware/qcom/x1e80100/microsoft/Denali
+    mkdir -p $out/lib/firmware/{qcom/x1e80100/microsoft/Denali,ath12k/WCN7850/hw2.0}
 
     cabextract -F qcdxkmsuc8380.mbn $qcdx8380
     mv qcdxkmsuc8380.mbn $out/lib/firmware/qcom/x1e80100/microsoft/qcdxkmsuc8380.mbn
@@ -39,6 +47,9 @@ stdenv.mkDerivation (finalAttrs: {
 
     cabextract -F qcadsp8380.mbn -d $out/lib/firmware/qcom/x1e80100/microsoft/Denali/ $adsp8380
     cabextract -F qccdsp8380.mbn -d $out/lib/firmware/qcom/x1e80100/microsoft/Denali/ $cdsp8380
+
+    python3 ${finalAttrs.bdencoder} --extract ${finalAttrs.board-2}
+    mv "bus=pci,vendor=17cb,device=1107,subsystem-vendor=17cb,subsystem-device=3378,qmi-chip-id=2,qmi-board-id=255.bin" $out/lib/firmware/ath12k/WCN7850/hw2.0/board.bin
 
     find "$out" -exec touch --date=2000-01-01 {} +
   '';
